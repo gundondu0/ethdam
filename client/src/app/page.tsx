631170728a60fa1d0790e79d22340f9dbb27bc7c
Victor Garbit <victor.garbit@gmail.com>
2024-04-12 21:11:55 +0200

    amount_input

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,12 +1,14 @@
 "use client"
 import { Box, Button, ButtonGroup } from '@chakra-ui/react'
 import TestComponent from '../components/test'
+import AmountInput from '../components/amount_input';
 
 export default function Home() {
   return (
     <div>
       <h1>Homepage
       </h1>
+      <AmountInput></AmountInput>
       <TestComponent></TestComponent>
       <Button bg={"primary"}></Button>
     </div>
