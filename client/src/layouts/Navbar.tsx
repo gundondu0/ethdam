1cc7fbfe97028c407b459e6db3e31c16daafe4c5
Chokhir <62755351+Chokhir@users.noreply.github.com>
2024-04-12 21:21:07 +0200

    Update Navbar component to fix import statements and add "Swap" link

--- client/src/layouts/Navbar.tsx
+++ client/src/layouts/Navbar.tsx
@@ -1,22 +1,19 @@
-import { Box, Link , Text} from "@chakra-ui/react";
+import { Box, Link, Text } from "@chakra-ui/react";
 import NextLink from "next/link";
 
+
 const Navbar = () => {
-  return (
-    <Box bg="primary" w="100%" p={4} color="white">
-      <NextLink href="/" passHref>
-        <Link mr={4}>Home</Link>
-      </NextLink>
+    return (
+        <Box bg="primary" w="100%" p={4} color="text">
+            <NextLink href="/" passHref>
+                <Link mr={4}>Home</Link>
+            </NextLink>
 
-      <NextLink href="/about" passHref>
-        <Link mr={4}>About</Link>
-      </NextLink>
-
-      <NextLink href="/contact" passHref>
-        <Link>Contact</Link>
-      </NextLink>
-    </Box>
-  );
+            <NextLink href="/swap" passHref>
+                <Link mr={4}>Swap</Link>
+            </NextLink>
+        </Box>
+    );
 };
 
 export default Navbar;
